d6677917a69b647536bce7350cb51fdd6a62f0da
Patrick Rubio <prubi16@wgu.edu>
2025-08-09 17:09:09 -0700

    Part B-1 (Created an enter screen) (in progress)
    - Created a main screen to enter from for the vacations and excursions

--- app/src/main/java/com/example/myapplication/UI/MainActivity.java
+++ app/src/main/java/com/example/myapplication/UI/MainActivity.java
@@ -1,12 +1,17 @@
-package com.example.myapplication;
+package com.example.myapplication.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, VacationList.class);
+                intent.putExtra("test","Information Sent");
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
