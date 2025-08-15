bb112c405efdec966fc6c5ebceb8b302af427593
Patrick Rubio <prubi16@wgu.edu>
2025-08-14 21:06:16 -0700

    Part B-1 (Added another floating action button and adding new activity) (in progress)
    - Created a new activity called Excursion Details
    - Created a linear layout for the Vacation Details

--- app/src/main/java/com/example/myapplication/UI/VacationList.java
+++ app/src/main/java/com/example/myapplication/UI/VacationList.java
@@ -54,6 +54,8 @@ public class VacationList extends AppCompatActivity {
         }
         if (item.getItemId()==android.R.id.home) {
             this.finish();
+//            Intent intent = new Intent(VacationList.this, VacationDetails.class);
+//            startActivity(intent);
             return true;
         }
         return true;
